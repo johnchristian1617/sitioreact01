dca387e9e9a919a31d41ffd6026eb57bb807ce9f
unknown <johnchristian1617@gmail.com>
2022-11-02 12:04:14 -0500

    version 1 de sitio react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MI PRIMERA PRUEBA JOHN PACHECO
         </a>
       </header>
     </div>
